508384a8cb8a9bb9ff2e6b23f2d6508db371f392
ItsEhsanMM <itsehsanmm@gmail.com>
2023-08-28 01:52:33 +0330

    add similar and recommend and topCast

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -22,7 +22,6 @@ const Details = () => {
       <div>
          <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
          <Cast data={credits?.cast} loading={creditsLoading} />
-
          <VideosSection data={data} loading={loading} />
          <Similar mediaType={mediaType} id={id} />
          <Recommendation mediaType={mediaType} id={id} />
